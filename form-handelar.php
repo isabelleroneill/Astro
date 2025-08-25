62a41314862a23207aed96f7c05018a61a7b41c0
isabelleroneill <isabelleroneill@gmail.com>
2025-08-24 19:20:08 -0700

    Update form-handelar.php

--- form-handelar.php
+++ form-handelar.php
@@ -1,26 +1,22 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(strip_tags($_POST['name']));
+    $visitor_email = htmlspecialchars(strip_tags($_POST['email']));
+    $subject = htmlspecialchars(strip_tags($_POST['subject']));
+    $message = htmlspecialchars(strip_tags($_POST['message']));
 
-$email_from = 'info@yourwebsite.com';
+    $to = "astrovisionariesinitiative@gmail.com";
+    $email_subject = "New Form Submission";
+    $email_body = "Name: $name\nEmail: $visitor_email\nSubject: $subject\nMessage:\n$message";
 
-$email_subject = 'New Form Submission';
+    $headers = "From: $visitor_email\r\n";
+    $headers .= "Reply-To: $visitor_email\r\n";
 
-$email_body = "User Name: $name.\n".
-               "User Email: $visitor_email.\n".
-                 "Subject: $subject.\n".
-                   "User Message: $message .\n";
-
-$to = 'astrovisionariesinitiative@gmail.com';
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
-
+    if(mail($to, $email_subject, $email_body, $headers)) {
+        header("Location: Contact.html");
+        exit;
+    } else {
+        echo "Error sending email.";
+    }
+}
 ?>
